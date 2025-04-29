3320da37345c6c4a9600bf3b924e448e5afeadb9
SaadNasir-Drago <snasir2194@gmail.com>
2025-04-29 10:52:36 +0600

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://www.pharmacyshop.company'],
 
     'allowed_origins_patterns' => [],
 
